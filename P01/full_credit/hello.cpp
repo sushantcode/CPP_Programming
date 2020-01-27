74cfb9240cc8bf90d047a3845775e7047318295b
sushantcode <sushantgupta2016@gmail.com>
2020-01-26 23:32:55 -0600

    P01 FullCredit My program compiles!

---
+++ P01/full_credit/hello.cpp
@@ -0,0 +1,12 @@
+/* This program simply prints "Hello [My name]!" on console output screen. */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<< "Hello, Sushant Gupta!" << endl;
+
+	return 0;
+}
