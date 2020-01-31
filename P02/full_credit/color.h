847cafe33c2ee3d1205b9e218a1d371d428b2021
sushantcode <sushantgupta2016@gmail.com>
2020-01-30 23:54:03 -0600

    P02 fc Header file added.

---
+++ P02/full_credit/color.h
@@ -0,0 +1,16 @@
+#ifndef _COLOR_H
+#define _COLOR_H
+
+#include <iostream>
+#include <string>
+
+class Color{
+	public:
+		Color(int red, int green, int blue);
+		~Color();
+		std::string to_string();
+		std::string colorize(std::string text)
+	private:
+		int _red, _green, _blue;
+}
+#endif
