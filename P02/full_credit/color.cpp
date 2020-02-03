0752b5d0ac68e75fa91cbdb18ccef477140ca0c0
Sushant <58162933+sushantcode@users.noreply.github.com>
2020-02-02 21:15:59 -0600

    P02 fc color.cpp updated

--- P02/full_credit/color.cpp
+++ P02/full_credit/color.cpp
@@ -1,6 +1,3 @@
-/* Sushant Gupta 1001520302 */
-/* This program prints given text and command in the representative color. */
-
 #include "color.h"
 
 Color::Color(int red, int green, int blue)
@@ -20,18 +17,4 @@ std::string Color::to_string(){
 		
 std::string Color::colorize(std::string text){
 	return "\033[38;2;"+std::to_string(_red)+";"+std::to_string(_green)+";"+std::to_string(_blue)+"m" + text + "\033[0m";
-}
-
-int main(){
-	Color red{255,0,0}, green{0,128,0}, purple{95,0,135};
-	std::cout << red.colorize("Red") << "\n" << green.colorize("Green") << "\n" << purple.colorize("Purple") << std::endl;
-	int user_red, user_green, user_blue;
-	std::cout << "Enter an integer representing Red: ";
-	std::cin >> user_red;
-	std::cout << "Enter an integer representing Green: ";
-	std::cin >> user_green;
-	std::cout << "Enter an integer representing Blue: ";
-	std::cin >> user_blue;
-	Color user_input{user_red,user_green,user_blue};
-	std::cout << user_input.colorize(user_input.to_string()) << std::endl;
 }
