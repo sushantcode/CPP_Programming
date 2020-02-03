0874c33259ac4a5a93a22cc985a9cef996644fa8
sushantcode <sushantgupta2016@gmail.com>
2020-02-03 13:40:03 -0600

    P02 eb Files added.

---
+++ P02/extreme_bonus/color.cpp
@@ -0,0 +1,38 @@
+#include "color.h"
+
+Color::Color(int red, int green, int blue)
+	: _red{red}, _green{green}, _blue{blue} {
+	if(_red > 255 || _red < 0) throw std::runtime_error{"Invalid red component!"};
+	if(_green > 255 || _green < 0) throw std::runtime_error{"Invalid green component!"};
+	if(_blue > 255 || _blue < 0) throw std::runtime_error{"Invalid blue component!"};
+}
+
+Color::~Color(){ }
+
+std::string Color::to_string(){
+	std::string string_out;
+	string_out = "(" + std::to_string(_red) + "," + std::to_string(_green) + "," + std::to_string(_blue) + ":" + std::to_string(Color::magnitude()) + ")";
+	return string_out;
+}
+		
+std::string Color::colorize(std::string text){
+	return "\033[38;2;"+std::to_string(_red)+";"+std::to_string(_green)+";"+std::to_string(_blue)+"m" + text + "\033[0m";
+}
+
+int Color::magnitude(){
+	return (0.21*_red + 0.72*_green + 0.07*_blue);
+}
+
+int Color::compare(const Color *rhs){
+	int thisObj = Color::magnitude();
+	int rhsObj = (0.21*rhs->_red + 0.72*rhs->_green + 0.07*rhs->_blue);
+	if(thisObj > rhsObj){
+		return 1;
+	}
+	else if(thisObj == rhsObj){
+	       return 0;
+	}
+	else{
+	       return -1;
+	}
+}
---
+++ P02/extreme_bonus/main.cpp
@@ -0,0 +1,16 @@
+/* Sushant Gupta 1001520302 */
+/* This program prints given text and command in the representative color including magnitude in color code. */
+
+#include "color.h"
+
+int main(){
+	Color red{255,0,0}, green{0,128,0}, purple{95,0,135};
+	std::cout << red.colorize("Red") << "\n" << green.colorize("Green") << "\n" << purple.colorize("Purple") << std::endl;
+	int user_red, user_green, user_blue;
+	std::cout << "Enter integers for red, green and blue components: ";
+	std::cin >> user_red >> user_green >> user_blue;
+	Color user_input{user_red,user_green,user_blue};
+	std::cout << user_input.colorize(user_input.to_string()) << std::endl;
+	Color rhs{156,48,215};
+	std::cout << user_input.compare(&rhs) << std::endl;
+}
