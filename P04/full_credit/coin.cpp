e164ea95f34b87ef140e76fc9932234e0cd73b93
sushantcode <sushantgupta2016@gmail.com>
2020-02-17 12:38:21 -0600

    P04 fc makefile created and updated.

--- P04/full_credit/coin.cpp
+++ P04/full_credit/coin.cpp
@@ -10,4 +10,23 @@ Coin::Coin(const Coin& rhs)
 	Logger::LOG("Coin::Coin copy constructor")
 }
 
+Coin& Coin::operator=(const Coin& rhs){
+	: _size{rhs._size}, _year{rhs._year}, _notes{new std::string{*(rhs._notes)}{
+	Logger::LOG("Coin::operator=")
+	return this;
+}
 
+Coin::~Coin(){
+	delete _notes;
+}
+
+Coin::add-note(std::string s){
+	if(!_notes){
+	_notes = new std::string{s};
+	}
+}
+
+std::ostream& operator<<(std::ostream& ost, Coin& coin){
+	ost << coin._year << " " << coin._size << "\n" << coin->_notes;
+	return ost;
+}
