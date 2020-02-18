78d5e8d943a3833901f8e7c189f7b51c65bb7a03
sushantcode <sushantgupta2016@gmail.com>
2020-02-17 19:33:57 -0600

    P04 bonus files added.

---
+++ P04/bonus/coin.cpp
@@ -0,0 +1,56 @@
+#include "coin.h"
+
+Coin::Coin(Coin_size size, Year year)
+	: _size{size}, _year{year}, _notes{nullptr} {
+	LOG("Coin::Coin");
+}
+
+Coin::Coin(const Coin& rhs)
+	: _size{rhs._size}, _year{rhs._year}, _notes{new std::string{*(rhs._notes)}}{
+	LOG("Coin::Coin copy constructor");
+}
+
+Coin& Coin::operator=(const Coin& rhs){
+	_size = rhs._size; 
+	_year = rhs._year;
+	_notes = new std::string{*(rhs._notes)};
+	LOG("Coin::operator=");
+	return *this;
+}
+
+Coin::~Coin(){
+	delete _notes;
+	LOG("Coin::~Coin");
+}
+
+void Coin::add_note(std::string s){
+	if(!_notes){
+		_notes = new std::string{s};
+	}
+	else{
+		*_notes = *_notes + s;
+	}
+}
+
+std::ostream& operator<<(std::ostream& ost,const Coin_size &coin)
+{
+	switch(coin)
+	{
+		case Coin_size::PENNY:
+			return ost << "penny";
+		case Coin_size::NICKEL:
+			return ost << "nickel";
+		case Coin_size::DIME:
+			return ost << "dime";
+		case Coin_size::QUARTER:
+			return ost << "quarter";
+		default:
+			throw std::runtime_error{"Coin value not available yet!"};
+	}
+	return ost;
+}
+
+std::ostream& operator<<(std::ostream& ost, Coin& coin){
+	ost << coin._year << " " << coin._size << "\n" << *(coin._notes);
+	return ost;
+}
---
+++ P04/bonus/coin.h
@@ -0,0 +1,28 @@
+#ifndef _COLOR_H
+#define _COLOR_H
+
+#include <iostream>
+#include <string>
+#include <ostream>
+#include "logger.h"
+
+
+typedef int Year;
+
+enum class Coin_size{PENNY, NICKEL, DIME, QUARTER};
+
+class Coin{
+	public:
+		Coin(Coin_size size, Year year);
+		Coin(const Coin& rhs);
+		Coin& operator=(const Coin& rhs);
+		~Coin();
+		void add_note(std::string s);
+		friend std::ostream& operator<<(std::ostream& ost, Coin& coin);
+    		friend std::istream& operator>>(std::istream& ist, Coin& coin);
+	private:
+		Coin_size _size;
+		Year _year;
+		std::string* _notes;
+};
+#endif
