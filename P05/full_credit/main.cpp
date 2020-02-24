29bc812a098fd538fbad84cf4a52871bf33a32b1
sushantcode <sushantgupta2016@gmail.com>
2020-02-23 23:24:44 -0600

    P05 fc main.cpp edited.

--- P05/full_credit/main.cpp
+++ P05/full_credit/main.cpp
@@ -1,17 +1,97 @@
 #include "product.h"
 #include "taxed.h"
 #include "taxfree.h"
+#include <vector>
 
 int main(){
-	Taxed snicker{"Snicker", 1.99}, pen{"Pen", 2.29}, Icecream{"Ice Cream", 3.99};
+	Taxed snicker{"Snicker", 1.99}, pen{"Pen", 2.29}, icecream{"Ice Cream", 3.99};
 	Taxfree milk{"Milk", 2.99}, water{"Water", 3.89}, bread{"Bread", 2.49};
-	std::string menu = R"(
+	std::vector <Product> cart;
+	const std::string menu = R"(
 	============================================
 			Welcome to the Our Store
 	============================================
 	0) Snicker ($1.99)
 	1) Pen ($2.29)
 	2) Ice Cream ($3.99)
-	3) 
-	)"
+	3) Milk ($2.99)
+	4) Water ($3.89)
+	5) Bread ($2.49)
+	
+	)";
+	int quantity = 1;
+	int index;
+	while(quantity != 0){
+		std::cout << "\nEnter quantity (0 to exit) and product index: " << std::endl;
+		std::cin >> quantity >> index;
+		switch(index){
+			case 0:
+				try{
+					if(quantity < 0) throw std::runtime_error("Invalid quantity!!");
+					cart.push_back(snicker);
+				}
+				catch (exception& e) {
+					std::cerr << "Invalid quantity!!" << e.what() << std::endl;
+				}
+				break;
+				
+			case 1:
+				try{
+					if(quantity < 0) throw std::runtime_error("Invalid quantity!!");
+					cart.push_back(pen);
+				}
+				catch (exception& e) {
+					std::cerr << "Invalid quantity!!" << e.what() << std::endl;
+				}
+				break;
+				
+			case 2:
+				try{
+					if(quantity < 0) throw std::runtime_error("Invalid quantity!!");
+					cart.push_back(icecream);
+				}
+				catch (exception& e) {
+					std::cerr << "Invalid quantity!!" << e.what() << std::endl;
+				}
+				break;
+				
+			case 3:
+				try{
+					if(quantity < 0) throw std::runtime_error("Invalid quantity!!");
+					cart.push_back(milk);
+				}
+				catch (exception& e) {
+					std::cerr << "Invalid quantity!!" << e.what() << std::endl;
+				}
+				break;
+				
+			case 4:
+				try{
+					if(quantity < 0) throw std::runtime_error("Invalid quantity!!");
+					cart.push_back(water);
+				}
+				catch (exception& e) {
+					std::cerr << "Invalid quantity!!" << e.what() << std::endl;
+				}
+				break;
+				
+			case 5:
+				try{
+					if(quantity < 0) throw std::runtime_error("Invalid quantity!!");
+					cart.push_back(bread);
+				}
+				catch (exception& e) {
+					std::cerr << "Invalid quantity!!" << e.what() << std::endl;
+				}
+				break;
+				
+			default:
+				std::cerr << "Invalid quantity!!" << e.what() << std::endl;
+		}
+		std::cout << "Current Order\n-------------------------------\n" << std::endl;
+		
+			
+	}
+	
+	
 }
