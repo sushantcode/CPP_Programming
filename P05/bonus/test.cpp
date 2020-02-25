4c7fc0b8db43e26c3a1e8e1d4d329f32c85f181a
sushantcode <sushantgupta2016@gmail.com>
2020-02-24 20:24:04 -0600

    P05 bonus error fixed.

--- P05/bonus/test.cpp
+++ P05/bonus/test.cpp
@@ -6,6 +6,25 @@
 
 int main(){
 	Taxed snicker{"Snicker", 1.99};
+	std::string name;
+	double cost;
+	double tax;
+	Taxed::set_tax_rate(0.0825);
+	snicker.tester(&name, &cost, &tax);
+	if(name != "Snicker"){
+		std::cerr << "FAIL: _name inherited attribute not initialized!! " << name << std::endl;
+		testresult = "FAIL";
+	}
+	if(cost != 1.99){
+		std::cerr << "FAIL: _cost inherited attribute not initialized!! " << cost << std::endl;
+		testresult = "FAIL";
+	}
+	if(tax != 0.0825){
+		std::cerr << "FAIL: _tax local attribute not initialized!! " << 0.0825 << std::endl;
+		testresult = "FAIL";
+	}
+	std::cout << tax << std::endl;
+	
 	std::stringstream ss;
 	ss << snicker;
 	std::string result1;
@@ -15,13 +34,10 @@ int main(){
 	result = result1 + result2;
 	std::string testresult;
 	if (result != "Snicker($1.99)"){
+		std::cerr << "FAIL:: Operator<< overload is not working!!" << std::endl;
 		testresult = "FAIL";
 	}
-	std::string name;
-	double cost;
-	double tax;
-	snicker.tester(&name, &cost, &tax);
-	std::cout << name << std::endl;
+	
 	return 0;
 	
 }
