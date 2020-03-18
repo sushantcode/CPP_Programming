4aa29097370c935002f267c02618482794748a96
sushantcode <sushantgupta2016@gmail.com>
2020-03-17 22:03:15 -0500

    elsa s01 customer class tested.

--- elsa/src/customer.cpp
+++ elsa/src/customer.cpp
@@ -1,9 +1,9 @@
-#include "custumer.h"
+#include "customer.h"
 
 Customer::Customer(std::string name, std::string phone, std::string email)
 	: _name{name}, _phone{phone}, _email{email} { }
 
 std::ostream& operator<<(std::ostream& ost, const Customer& cust){
-	ost << cust._name << cust._phone << cust._email;
+	ost << cust._name << " " << cust._phone << " " << cust._email;
 	return ost;
 }
