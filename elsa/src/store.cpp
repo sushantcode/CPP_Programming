59f380ca44341a155291ff875ee7a6a29a5e9011
sushantcode <sushantgupta2016@gmail.com>
2020-03-19 20:57:46 -0500

    elsa s01 desktop class some errors fixed.

--- elsa/src/store.cpp
+++ elsa/src/store.cpp
@@ -1,6 +1,5 @@
 #include "store.h"
 
-
 void Store::add_customer(Customer& customer){
 	customers.push_back(customer);
 }
@@ -14,7 +13,7 @@ Customer& Store::customer(int index){
 }
 
 void Store::add_option(Options& option){
-	options.push_back(option);
+	options.push_back(&option);
 }
 
 int Store::num_options(){
@@ -22,29 +21,25 @@ int Store::num_options(){
 }
 
 Options& Store::option(int index){
-	return options.at(index);
+	return *options.at(index);
 }
 
 int Store::new_desktop(){
-	Desktop newDesktop = new Desktop();
-	desktops.push_back(newDesktop);
-	int index;
-	std::vector<Desktop>::iterator it = std::find(desktops.begin(), desktops.end(), newDesktop);
-	if (it != desktops.end()){
-		index = it - desktops.begin();
-	}
-	return index;
+	int curr_size = desktops.size();
+	Desktop *newDesktop = new Desktop();
+	desktops.push_back(*newDesktop);
+	return curr_size + 1;
 }
 
 void Store::add_option(int option, int desktop){
 	Desktop target = desktops.at(desktop);
-	target.add_option(options.at(option));
+	target.add_option(*options.at(option));
 }
 
 int Store::num_desktop(){
 	return desktops.size();
 }
 
-Desktop& desktop(int index){
+Desktop& Store::desktop(int index){
 	return desktops.at(index);
 }
--- elsa/src/test.cpp
+++ elsa/src/test.cpp
@@ -1,13 +1,15 @@
 #include "customer.h"
 #include "store.h"
 #include "options.h"
+#include "desktop.h"
 
 int main(){
 	Store elsa;
 	Options opt1{"Premium", 2100.00}, opt2{"Generic", 850.00};
-	elsa.Store::add_option(opt1);
-	elsa.Store::add_option(opt2);
-	int num = elsa.Store::num_options();
-	std::cout << elsa.Store::option(1) << std::endl;
+	elsa.add_option(opt1);
+	elsa.add_option(opt2);
+	/*int newDesk = elsa.add_option
+	int num = elsa.num_options();*/
+	std::cout << elsa.option(1) << std::endl;
 	return 0;
 }
