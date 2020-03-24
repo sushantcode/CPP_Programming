db434738de826813e4e48a2de311a723b7f77c3f
sushantcode <sushantgupta2016@gmail.com>
2020-03-23 21:09:23 -0500

    elsa s02 mainwin files created.

---
+++ elsa/src/mainwin.h
@@ -0,0 +1,32 @@
+#ifndef _MAINWIN_H
+#define _MAINWIN_H
+
+#include <gtkmm.h>
+
+class Mainwin : public Gtk::window{
+	public:
+		Mainwin();
+		virtual ~Mainwin();
+		
+	protected:
+		void on_quit_click();
+		void on_view_peripheral_click();
+		void on_view_desktop_click();
+		void on_view_order_click();
+		void on_view_customer_click();
+		void on_insert_peripheral_click();
+		void on_insert_desktop_click();
+		void on_insert_order_click();
+		void on_insert_customer_click();
+		void 0n_about_click();
+		std::string get_string(std::string promt);
+		double get_double(std::string prompt);
+		int get_in(std::string prompt);
+		void set_data(std::string s);
+		void set_msg(std::string s);
+		
+	private:
+		Store *store;
+		Gtk::Label *data;
+		Gtk::Label *msg;
+}
